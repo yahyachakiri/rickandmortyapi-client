b8e673c9803a6b60a0da43a20534040a155b7673
Yahya CHAKIRI <yahyachakiridev@gmail.com>
2024-06-06 15:32:30 +0100

    fix: ignore container type

--- src/components/Layout/Container.tsx
+++ src/components/Layout/Container.tsx
@@ -7,5 +7,7 @@ interface props {
 export const Container = ({ children, className, as = "div" }: props) => {
   const Component = as ?? "div";
 
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
   return <Component className={`${className ?? ""} mx-auto max-w-[1200px] px-5 sm:px-10 lg:px-15 xl:px-20`}>{children}</Component>;
 };
